b3103273c41d0344e1810b9778f244243c7b48b1
csupreme19 <csupreme19@gmail.com>
2022-09-28 19:46:43 +0900

    예제 도메인 설정과 기초 테스트

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -2,27 +2,49 @@ package study.datajpa.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+import static javax.persistence.FetchType.LAZY;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = PROTECTED)
+@ToString(exclude = {"team"})
 public class Member {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
+
     private String username;
+    private Integer age;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
     }
 
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null) {
+            changeTeam(team);
+        }
+    }
+
     public void changeUsername(String username) {
         this.username = username;
+    }
+
+    public void changeTeam(Team team) {
+        team.getMembers().add(this);
+        this.team = team;
     }
 }
---
+++ src/main/java/study/datajpa/entity/Team.java
@@ -0,0 +1,34 @@
+package study.datajpa.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@ToString(exclude = {"members"})
+public class Team {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
