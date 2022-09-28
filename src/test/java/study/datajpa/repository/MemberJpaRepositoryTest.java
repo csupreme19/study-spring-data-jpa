0d31e9d3ccdc97de2042f67b3522a4a2cbb97492
csupreme19 <csupreme19@gmail.com>
2022-09-28 19:22:44 +0900

    JPA Repository vs Spring Data JPA Repository

---
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,33 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+@Rollback(false)
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    void testMember() {
+        Member member = new Member("member1");
+        Member saveMember = memberJpaRepository.save(member);
+        Member findMember = memberJpaRepository.find(saveMember.getId());
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+        // 같은 트랜잭션 내 영속성 컨텍스트 동일성 보장
+        assertThat(findMember).isEqualTo(member);
+    }
+
+}
---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+@Rollback(false)
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void testMember() {
+        Member member = new Member("member1");
+        Member saveMember = memberRepository.save(member);
+        Member findMember = memberRepository.findById(saveMember.getId()).get();
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+        // 같은 트랜잭션 내 영속성 컨텍스트 동일성 보장
+        assertThat(findMember).isEqualTo(member);
+    }
+
+}
